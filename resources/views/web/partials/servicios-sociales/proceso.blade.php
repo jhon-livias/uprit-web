e03898af716799e186cc115f2a2049361124e31c
Jhon Livias <jhonlivias3@gmail.com>
2026-08-12 22:46:11 -0500

    fix proceso

--- resources/views/web/partials/servicios-sociales/proceso.blade.php
+++ resources/views/web/partials/servicios-sociales/proceso.blade.php
@@ -1,22 +1,64 @@
 @php
     $steps = [
-        ['icon' => 'mdi:bullhorn-outline', 'title' => 'Convocatoria', 'date' => 'Del 1 al 10 de agosto'],
-        ['icon' => 'mdi:file-document-edit-outline', 'title' => 'Presentación', 'date' => 'Del 11 al 16 de agosto'],
-        ['icon' => 'mdi:inbox-arrow-down-outline', 'title' => 'Recepción', 'date' => 'Del 17 al 19 de agosto'],
-        ['icon' => 'mdi:clipboard-check-outline', 'title' => 'Verificación', 'date' => 'Del 20 al 22 de agosto'],
-        ['icon' => 'mdi:magnify', 'title' => 'Evaluación', 'date' => 'Del 23 al 27 de agosto'],
-        ['icon' => 'mdi:home-search-outline', 'title' => 'Visita', 'date' => 'Del 28 al 31 de agosto'],
-        ['icon' => 'mdi:account-voice', 'title' => 'Entrevista', 'date' => 'Del 1 al 3 de septiembre'],
-        ['icon' => 'mdi:gavel', 'title' => 'Dictamen', 'date' => 'Del 4 al 6 de septiembre'],
-        ['icon' => 'mdi:trophy-outline', 'title' => 'Resultados', 'date' => '8 de septiembre'],
-        ['icon' => 'mdi:file-sign', 'title' => 'Formalización', 'date' => 'Del 9 al 12 de septiembre'],
+        [
+            'icon' => 'mdi:file-document-edit-outline',
+            'title' => 'Presentación',
+            'date' => 'Del 17 al 19 de agosto',
+        ],
+        [
+            'icon' => 'mdi:email-arrow-up-outline',
+            'title' => 'Recepción',
+            'date' => 'Del 20 al 25 de agosto',
+        ],
+        [
+            'icon' => 'mdi:clipboard-check-outline',
+            'title' => 'Subsanación',
+            'date' => '2 días para expedientes incompletos',
+        ],
+        [
+            'icon' => 'mdi:magnify',
+            'title' => 'Evaluación',
+            'date' => 'Del 28 al 31 de agosto',
+        ],
+        [
+            'icon' => 'mdi:shield-check-outline',
+            'title' => 'Verificación',
+            'date' => 'Del 28 de agosto al 1 de septiembre',
+        ],
+        [
+            'icon' => 'mdi:clipboard-edit-outline',
+            'title' => 'Cumplimiento',
+            'date' => 'Del 1 al 5 de septiembre',
+        ],
+        [
+            'icon' => 'mdi:file-chart-outline',
+            'title' => 'Informe',
+            'date' => 'Del 7 al 9 de septiembre',
+        ],
+        [
+            'icon' => 'mdi:account-check-outline',
+            'title' => 'Aprobación',
+            'date' => 'Del 10 al 14 de septiembre',
+        ],
+        [
+            'icon' => 'mdi:file-certificate-outline',
+            'title' => 'Resolución',
+            'date' => 'Del 18 al 20 de septiembre',
+        ],
+        [
+            'icon' => 'mdi:email-seal-outline',
+            'title' => 'Notificación',
+            'date' => 'Del 19 al 22 de septiembre',
+        ],
     ];
 @endphp
 
 <section class="sociales-proceso" id="proceso">
     <div class="container">
-        <header class="sociales-heading">
-            <h2 class="sociales-heading__title">Proceso de Solicitud de Becas y Medias Becas</h2>
+        <header class="sociales-proceso__header">
+            <h2 class="sociales-proceso__title">
+                Proceso de Solicitud de <span>Becas y Medias Becas</span>
+            </h2>
         </header>
 
         <ol class="sociales-steps">
@@ -33,9 +75,9 @@
         </ol>
 
         <div class="sociales-proceso__cta">
-            <a href="#documentacion" class="sociales-btn sociales-btn--solid">
-                <iconify-icon icon="mdi:file-document-outline" aria-hidden="true"></iconify-icon>
+            <a href="#documentacion" class="sociales-proceso__btn">
                 Ver requisitos completos
+                <iconify-icon icon="mdi:file-document-outline" aria-hidden="true"></iconify-icon>
             </a>
         </div>
     </div>
