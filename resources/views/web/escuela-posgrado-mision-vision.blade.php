e9591aef2e9438828d794b12ae04eecd95398ffc
Jhon Livias <jhonlivias3@gmail.com>
2026-08-19 10:12:47 -0500

    fix posgrado

--- resources/views/web/escuela-posgrado-mision-vision.blade.php
+++ resources/views/web/escuela-posgrado-mision-vision.blade.php
@@ -2,40 +2,57 @@
 @section('content')
 @include('web.partials.breadcrumb')
 
-<section class="privacy-policy-area">
+<section class="privacy-policy-area posgrado-mvv-page">
     <div class="container">
         <div class="row row--30">
             <div class="col-lg-8">
                 <div class="privacy-policy">
-                    <div class="text-block">
-                        <div class="about-mvv">
-                            <ul class="nav nav-tabs about-mvv__tabs" role="tablist">
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#posgrado-mision" type="button" role="tab" aria-selected="true">
-                                        <iconify-icon icon="mdi:target" aria-hidden="true"></iconify-icon>
-                                        Misión
-                                    </button>
-                                </li>
-                                <li class="nav-item" role="presentation">
-                                    <button class="nav-link" data-bs-toggle="tab" data-bs-target="#posgrado-vision" type="button" role="tab" aria-selected="false">
-                                        <iconify-icon icon="mdi:eye-outline" aria-hidden="true"></iconify-icon>
-                                        Visión
-                                    </button>
-                                </li>
-                            </ul>
-                            <div class="tab-content about-mvv__content">
-                                <div class="tab-pane fade show active" id="posgrado-mision" role="tabpanel">
-                                    <div class="about-mvv__panel">
-                                        <p>La Universidad Privada de Trujillo brinda una formación profesional integral y humanista de excelencia, generando conocimiento científico y tecnológico que contribuye al desarrollo armónico y sostenible de la sociedad, abordando los problemas, retos y desafíos sociales, económicos, culturales, y ambientales que la aquejan con un alto espíritu de responsabilidad social y ética.</p>
-                                    </div>
-                                </div>
-                                <div class="tab-pane fade" id="posgrado-vision" role="tabpanel">
-                                    <div class="about-mvv__panel">
-                                        <p>Al 2027 nuestra universidad está integrada al mundo, reconocida por su aporte significativo al desarrollo humano, al responder a las demandas, retos y desafíos del entorno, favoreciendo con ello la competitividad y la sostenibilidad a través de la excelencia de sus egresados y la investigación e innovación actuando con un alto sentido de responsabilidad social y ética.</p>
-                                    </div>
+                    <header class="posgrado-mvv-page__header text-center">
+                        <span class="posgrado-mvv-page__eyebrow">Escuela de Posgrado</span>
+                        <h2 class="posgrado-mvv-page__title">Misión y Visión</h2>
+                        <p class="posgrado-mvv-page__lead">
+                            Los principios que orientan la formación de excelencia, la investigación y el compromiso social de nuestros programas de posgrado.
+                        </p>
+                    </header>
+
+                    <div class="posgrado-mvv-page__grid">
+                        <article class="posgrado-mvv-card posgrado-mvv-card--mision">
+                            <div class="posgrado-mvv-card__head">
+                                <span class="posgrado-mvv-card__icon" aria-hidden="true">
+                                    <iconify-icon icon="mdi:target"></iconify-icon>
+                                </span>
+                                <div>
+                                    <h3 class="posgrado-mvv-card__title">Misión</h3>
+                                    <p class="posgrado-mvv-card__subtitle">Nuestro propósito institucional</p>
                                 </div>
                             </div>
-                        </div>
+                            <div class="posgrado-mvv-card__body">
+                                <p>La Universidad Privada de Trujillo brinda una formación profesional integral y humanista de excelencia, generando conocimiento científico y tecnológico que contribuye al desarrollo armónico y sostenible de la sociedad, abordando los problemas, retos y desafíos sociales, económicos, culturales, y ambientales que la aquejan con un alto espíritu de responsabilidad social y ética.</p>
+                            </div>
+                        </article>
+
+                        <article class="posgrado-mvv-card posgrado-mvv-card--vision">
+                            <div class="posgrado-mvv-card__head">
+                                <span class="posgrado-mvv-card__icon" aria-hidden="true">
+                                    <iconify-icon icon="mdi:eye-outline"></iconify-icon>
+                                </span>
+                                <div>
+                                    <h3 class="posgrado-mvv-card__title">Visión</h3>
+                                    <p class="posgrado-mvv-card__subtitle">Horizonte institucional</p>
+                                </div>
+                                <span class="posgrado-mvv-card__badge">2027</span>
+                            </div>
+                            <div class="posgrado-mvv-card__body">
+                                <p>Al 2027 nuestra universidad está integrada al mundo, reconocida por su aporte significativo al desarrollo humano, al responder a las demandas, retos y desafíos del entorno, favoreciendo con ello la competitividad y la sostenibilidad a través de la excelencia de sus egresados y la investigación e innovación actuando con un alto sentido de responsabilidad social y ética.</p>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div class="posgrado-mvv-page__cta">
+                        <a href="{{ route('escuela-posgrado') }}" class="posgrado-mvv-page__cta-link">
+                            Conocer la Escuela de Posgrado
+                            <iconify-icon icon="mdi:arrow-right" aria-hidden="true"></iconify-icon>
+                        </a>
                     </div>
                 </div>
             </div>
